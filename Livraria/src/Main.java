8dbf83cbb94ebf7cb9c870b6a4fae8ef10f086fd
CowboyBepoppy <104656744+CowboyBepoppy@users.noreply.github.com>
2022-06-10 00:17:29 -0300

    Add files via upload

---
+++ Livraria/src/Main.java
@@ -0,0 +1,35 @@
+import classes.guardados.Estante;
+import classes.itens.DVD;
+import classes.itens.Livro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Estante estante = new Estante(5);
+        DVD dvd = new DVD();
+        Livro livro = new Livro();
+
+    
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+/*public class Main {
+    public static void main(String[] args) {
+        Livro l = new Livro();
+        l.setTitulo("Harry Potter e a Autora Transfóbica");
+        l.avaliar();
+        l.avaliar();
+        l.avaliar();
+        System.out.println(l.getTotalRating());
+    }
+}*/
